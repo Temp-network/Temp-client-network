d6348c17aeaad7788950f3ae08f0a64edd8b4f24
Carlos Us Santiago <cdus12@gmail.com>
2019-09-04 16:10:37 -0700

    passing test and working route for GET, which gets all monitors

---
+++ test/routes/monitor.test.js
@@ -0,0 +1,38 @@
+require('dotenv').config();
+
+const request = require('supertest');
+const app = require('../../lib/app');
+const connect = require('../../lib/utils/connect');
+const mongoose = require('mongoose');
+const seedData = require('../routes/seedData');
+
+describe('Monitor routes', () => {
+  beforeAll(() => {
+    connect();
+  });
+
+  beforeEach(() => {
+    return mongoose.connection.dropDatabase();
+  });
+
+  afterAll(() => {
+    return mongoose.connection.close();
+  });
+
+  it('get all Monitors', async() => {
+    const { monitors } = await seedData();
+
+    return request(app)
+      .get('/api/v1/monitors')
+      .then(res => {
+        monitors.forEach(monitor => {
+          expect(res.body).toContainEqual(JSON.parse(JSON.stringify(monitor)));
+        });
+        expect(res.body).toHaveLength(20);
+      });
+  });
+
+  //monitor by id
+  //get all monitors
+  // get all temp by monitor id
+});
